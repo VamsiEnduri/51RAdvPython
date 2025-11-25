7e7c77f88730182c303e32a100b8f9f1be76662d
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2025-11-25 15:42:35 +0530

    django views & url&templates req_res_cycle

---
+++ req_res_views_urls_temp/req_res_cycle/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import home,about,contact
+# urlpatterns=[
+#     path("",home),
+#     path("about/",about),
+#     path("contact/",contact)
+# ]
+
+
+urlpatterns=[
+    path("",home),
+    path("about/",about),
+    path("contact/",contact)
+]
---
+++ req_res_views_urls_temp/req_res_cycle/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+
+def about(request):
+    return render(request,"about.html")
+
+def contact(request):
+    return render(request,"contact.html")    
+# def home(request):
+#     return HttpResponse("this is homeview")
+# def about(request):
+#     return HttpResponse("this is about view")    
+
+# def contact(request):
+#     return HttpResponse("this is contact view") 
